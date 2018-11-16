e03a5a6dcceaba2edc940c89acdc3e5e28138c62
mahdihs76 <mahdihasanzadeh76@gmail.com>
2018-11-16 16:51:30 +0330

    Syscall completed with asm volatile to fetch CpuId

--- newSyscall/newSyscall.c
+++ newSyscall/newSyscall.c
@@ -7,6 +7,21 @@
 
 asmlinkage long sys_getCpuId(void)
 {
-	return 9092201202;
+  	unsigned int eax, ebx, ecx, edx;
+	eax = 3; /* EAX=3: Processor Serial Number */
+	ebx = 0;
+	ecx = 0;
+	edx = 0;
+ 	/* For Intel Pentium III CPUs, the serial number is returned in the EDX:ECX registers.
+	 For Transmeta Efficeon CPUs, it is returned in the EBX:EAX registers.
+	 And for Transmeta Crusoe CPUs, it is returned in the EBX register only. */
+        asm volatile("cpuid"
+            : "=a" (*eax),
+              "=b" (*ebx),
+              "=c" (*ecx),
+              "=d" (*edx)
+            : "0" (*eax), "2" (*ecx));
+
+	return ebx; /* Transmeta Efficeon CPU ID */
 }
 
